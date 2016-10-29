3c8b12670ead0045a2b99019680d8522928f1500
Jazzy Jazz <you@example.com>
2016-10-29 13:45:04 +0300

    Process ...

--- www/writelog.js
+++ www/writelog.js
@@ -2,6 +2,7 @@ var exec = require('cordova/exec');
 
 module.exports = {
 	write: function(arg0, success, error) {
+		console.log('IN WRITE');
 		exec(success, error, "WriteLog", "writeLog", [arg0]);
 	}
 };
