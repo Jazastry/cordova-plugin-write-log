a1f108b697df74685671a05350f97906993a77b0
Jazzy Jazz <you@example.com>
2016-10-29 11:02:45 +0300

    JS file upgrade.

--- www/GetLog.js
+++ www/GetLog.js
@@ -1,5 +1,7 @@
 var exec = require('cordova/exec');
 
-exports.getLog = function(arg0, success, error) {
-    exec(success, error, "GetLog", "getLog", [arg0]);
+module.exports = {
+    getLog: function(arg0, success, error) {
+        exec(success, error, "GetLog", "getLog", [arg0]);
+    }
 };
